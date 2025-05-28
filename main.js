23d2c1f1851c1fb8f910d076afe431c630fa08dd
sgleisner <sgleisner@gmail.com>
2025-05-28 17:08:38 +0000

    feat: add and test functions for reference and wrapper types

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-function getResponse() {
+const { JSDOM } = require("jsdom");
+
+function getResponseWithArray() {
   return new Response(
     JSON.stringify({
       message: "Hello, Node.js Response!",
@@ -7,11 +9,47 @@ function getResponse() {
   );
 }
 
-function getObject() {
+function getObjectWithArray() {
   return {
     message: "Hello, Object!",
     details: ["apple", "banana", "cherry"],
   };
 }
 
-module.exports = { getResponse, getObject };
+function getResponseWithReferenceType() {
+  return new Response(
+    JSON.stringify({
+      message: "Hello, Node.js Response!",
+      details: new Map(),
+    })
+  );
+}
+
+function getObjectWithReferenceType() {
+  return {
+    message: "Hello, Object!",
+    details: new Map(),
+  };
+}
+
+function getObjectWithCrossRealmPrimitiveWrapperType() {
+  const dom = new JSDOM();
+  const win = dom.window;
+  return new win.String("Hello, String!");
+}
+
+function getObjectWithPrimitiveWrapperType() {
+  return {
+    message: "Hello, Object!",
+    details: new String("Hello, String!"),
+  };
+}
+
+module.exports = {
+  getResponseWithArray,
+  getObjectWithArray,
+  getResponseWithReferenceType,
+  getObjectWithReferenceType,
+  getObjectWithCrossRealmPrimitiveWrapperType,
+  getObjectWithPrimitiveWrapperType,
+};
--- main.test.js
+++ main.test.js
@@ -1,8 +1,8 @@
-const { getResponse, getObject } = require("./main");
+const examples = require("./main");
 
 describe("getResponse", () => {
-  it("should return the correct JSON response", async () => {
-    const response = getResponse();
+  it("should return the correct JSON response with an Array", async () => {
+    const response = examples.getResponseWithArray();
     const data = await response.json();
 
     expect(response.status).toBe(200);
@@ -12,12 +12,50 @@ describe("getResponse", () => {
     });
   });
 
-  it("should return the correct Object", async () => {
-    const object = getObject();
+  it("should return the correct Object with an Array", async () => {
+    const object = examples.getObjectWithArray();
 
     expect(object).toMatchObject({
       message: expect.any(String),
       details: expect.any(Array),
+    });
+  });
+
+  it("should return the correct JSON response with a reference type", async () => {
+    const response = examples.getResponseWithReferenceType();
+    const data = await response.json();
+
+    expect(response.status).toBe(200);
+    expect(data).toMatchObject({
+      message: expect.any(String),
+      details: expect.any(Map),
+    });
+  });
+
+  it("should return the correct Object with a reference type", async () => {
+    const object = examples.getObjectWithReferenceType();
+
+    expect(object).toMatchObject({
+      message: expect.any(String),
+      details: expect.any(Map),
+    });
+  });
+
+  it("should return the correct object with a cross-realm primitive wrapper type", async () => {
+    const object = examples.getObjectWithCrossRealmPrimitiveWrapperType();
+
+    expect(object).toMatchObject({
+      message: expect.any(String), // Succeeds, since `message` is a primitive string.
+      details: expect.any(String), // Fails, since `details` is a cross-realm String object.
+    });
+  });
+
+  it("should return the correct object with a same-realm primitive wrapper type", async () => {
+    const object = examples.getObjectWithPrimitiveWrapperType();
+
+    expect(object).toMatchObject({
+      message: expect.any(String),
+      details: expect.any(String),
     });
   });
 });
